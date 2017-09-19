afe723c1fd2fa574e2b9ed0794d1efda51eb40f2
Ben Thorner <ben.thorner@bt.com>
2017-09-19 08:51:28 +0100

    Add test for JSON fix

--- spec/util/builder_spec.js
+++ spec/util/builder_spec.js
@@ -1,22 +1,33 @@
 import {Builder} from '../../src/util/builder'
 
 describe('Builder', function () {
-  beforeEach(function () {
-    this.subject = new Builder({
-      mathScratchPad: 'foo = data[1].datapoints[1]',
-      mathDisplayValue: 'foo', mathColorValue: 'foo + 1'
-    })
-  })
-
   describe('call', function () {
     it('returns a box with evaluated expressions', function () {
-      var seriesList = [{ datapoints: [0, 1, 2] }]
+      var subject = new Builder({
+        mathScratchPad: 'foo = data[1].datapoints[1]',
+        mathDisplayValue: 'foo', mathColorValue: 'foo + 1'
+      })
 
       var expected = {
         scratchPad: 0, displayValue: 0, colorValue: 1
       }
 
-      expect(this.subject.call(seriesList)).toEqual(expected)
+      var seriesList = [{ datapoints: [0, 1, 2] }]
+      expect(subject.call(seriesList)).toEqual(expected)
+    })
+
+    it('copes when the data are not plain objects', function () {
+      var subject = new Builder({
+        mathScratchPad: 'data.options.mathScratchPad',
+        mathDisplayValue: '1', mathColorValue: '1'
+      })
+
+      var expected = {
+        scratchPad: 'data.options.mathScratchPad',
+        displayValue: 1, colorValue: 1
+      }
+
+      expect(subject.call(subject)).toEqual(expected)
     })
   })
 })
