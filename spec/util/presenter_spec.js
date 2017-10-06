3fbb661e0e7065c549ca5689eee254cfdee36306
Ben Thorner <ben.thorner@bt.com>
2017-10-06 17:16:53 +0100

    Fix issue with negative thresholds

--- spec/util/presenter_spec.js
+++ spec/util/presenter_spec.js
@@ -10,7 +10,7 @@ describe('Presenter', function () {
   })
 
   describe('call', function () {
-    var box = { colorValue: 100 }
+    var box = { colorValue: 99.5 }
 
     describe('when there are no thresholds', function () {
       it('assigns the default color', function () {
@@ -21,7 +21,7 @@ describe('Presenter', function () {
 
     describe('when the thresholds are too high', function () {
       it('assigns the default color', function () {
-        this.panel.thresholds.push({ value: 101, color: 'color' })
+        this.panel.thresholds.push({ value: '101', color: 'color' })
         this.subject.call(box)
         expect(box.color).toEqual('default')
       })
@@ -29,7 +29,7 @@ describe('Presenter', function () {
 
     describe('when a threshold value is reached', function () {
       it('assigns the threshold color', function () {
-        this.panel.thresholds.push({ value: 100, color: 'color' })
+        this.panel.thresholds.push({ value: '99', color: 'color' })
         this.subject.call(box)
         expect(box.color).toEqual('color')
       })
@@ -37,10 +37,11 @@ describe('Presenter', function () {
 
     describe('when several thresholds are reached', function () {
       it('uses the closest threshold color', function () {
-        this.panel.thresholds.push({ value: 99, color: 'color1' })
-        this.panel.thresholds.push({ value: 89, color: 'color2' })
+        this.panel.thresholds.push({ value: '-99', color: 'color1' })
+        this.panel.thresholds.push({ value: '-79', color: 'color3' })
+        this.panel.thresholds.push({ value: '-89', color: 'color2' })
         this.subject.call(box)
-        expect(box.color).toEqual('color1')
+        expect(box.color).toEqual('color3')
       })
     })
   })
